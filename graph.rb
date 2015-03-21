f82a484698c833e95fd82325f6f162b12d2b622a
Weldon <wjsams@gmail.com>
2015-03-20 20:52:07 -0400

    Created a continuous graph of y=x^2 however the left half of the parabola is only plotted.

--- graph.rb
+++ graph.rb
@@ -36,14 +36,32 @@ class RubyGraph
         canvas = PNG::Canvas.new(@x_pos_units + @x_neg_units + 1, @y_pos_units + @y_neg_units + 1);
         x_start.upto(x_end) do |x|
             y_start.upto(y_end) do |y|
-                # 255:white 0:black
+                # 255,255,255:white 0,0,0:black
                 r = g = b = x == 0 || y == 0 ? 0 : 255
+
                 if y == fn(x)
+                    # 255,0,0:red
                     r = 255
                     g = 0
                     b = 0
                 end
+
                 canvas[x + @x_neg_units, y + @y_neg_units] = PNG::Color.new(r, g, b)
+            end
+
+            if x < x_end
+                y_fill_start = fn(x) + 1
+                y_fill_end = fn(x + 1) - 1
+                y_fill_mid = y_fill_start + ((y_fill_end - y_fill_start) / 2)
+                y_fill_start.upto(y_fill_end) do |y_fill|
+                    if y_fill >= y_start && y_fill <= y_end
+                        if y_fill <= y_fill_mid
+                            canvas[x + @x_neg_units, y_fill + @y_neg_units] = PNG::Color.new(255, 0, 0)
+                        else
+                            canvas[x + @x_neg_units, y_fill + @y_neg_units] = PNG::Color.new(255, 0, 0)
+                        end
+                    end
+                end
             end
         end
         png = PNG.new(canvas)
