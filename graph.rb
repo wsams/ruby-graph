f385e3130d6652329633695acd0d3343540a9d44
Weldon <wjsams@gmail.com>
2015-03-20 21:42:49 -0400

    Left half of parabola is nearly continuous, however it is not 100% accurate.

--- graph.rb
+++ graph.rb
@@ -7,19 +7,19 @@ class RubyGraph
     attr_accessor :y_precision
 
     # These values determine where the X and Y axis lies.
-    attr_accessor :x_pos_units
-    attr_accessor :y_pos_units
-    attr_accessor :x_neg_units
-    attr_accessor :y_neg_units
+    attr_accessor :x_pos_offset
+    attr_accessor :y_pos_offset
+    attr_accessor :x_neg_offset
+    attr_accessor :y_neg_offset
 
     def initialize
         @x_precision = 1
         @y_precision = 1
 
-        @x_pos_units = 20
-        @x_neg_units = 20
-        @y_pos_units = 200
-        @y_neg_units = 20
+        @x_pos_offset = 20
+        @x_neg_offset = 20
+        @y_pos_offset = 200
+        @y_neg_offset = 20
     end
 
     # Internal: This is where you define the equation you want to graph
@@ -27,13 +27,40 @@ class RubyGraph
         x ** 2
     end
 
+    def fill_empty_y(x, x_end, y_start, y_end, canvas)
+        if x < x_end
+            if x <= 0
+                y_fill_start = (fn(x + 1) + 1)
+                y_fill_end = (fn(x) - 1)
+            else
+                y_fill_start = fn(x) + 1
+                y_fill_end = fn(x + 1) - 1
+            end
+            y_fill_mid = y_fill_start + ((y_fill_end - y_fill_start) / 2)
+            y_fill_start.upto(y_fill_end) do |y_fill|
+                if y_fill >= y_start && y_fill <= y_end
+                    if x < 0
+                        x_val = x
+                    else
+                        x_val = x
+                    end
+                    if y_fill <= y_fill_mid
+                        canvas[x_val + @x_neg_offset, y_fill + @y_neg_offset] = PNG::Color.new(255, 0, 0)
+                    else
+                        canvas[x_val + @x_neg_offset, y_fill + @y_neg_offset] = PNG::Color.new(255, 0, 0)
+                    end
+                end
+            end
+        end
+    end
+
     # Public: Call this method to produce graph.png
     def graph
-        x_start = -1 * @x_neg_units;
-        x_end = @x_pos_units;
-        y_start = -1 * @y_neg_units;
-        y_end = @y_pos_units;
-        canvas = PNG::Canvas.new(@x_pos_units + @x_neg_units + 1, @y_pos_units + @y_neg_units + 1);
+        x_start = -1 * @x_neg_offset;
+        x_end = @x_pos_offset;
+        y_start = -1 * @y_neg_offset;
+        y_end = @y_pos_offset;
+        canvas = PNG::Canvas.new(@x_pos_offset + @x_neg_offset + 1, @y_pos_offset + @y_neg_offset + 1);
         x_start.upto(x_end) do |x|
             y_start.upto(y_end) do |y|
                 # 255,255,255:white 0,0,0:black
@@ -46,23 +73,10 @@ class RubyGraph
                     b = 0
                 end
 
-                canvas[x + @x_neg_units, y + @y_neg_units] = PNG::Color.new(r, g, b)
+                canvas[x + @x_neg_offset, y + @y_neg_offset] = PNG::Color.new(r, g, b)
             end
 
-            if x < x_end
-                y_fill_start = fn(x) + 1
-                y_fill_end = fn(x + 1) - 1
-                y_fill_mid = y_fill_start + ((y_fill_end - y_fill_start) / 2)
-                y_fill_start.upto(y_fill_end) do |y_fill|
-                    if y_fill >= y_start && y_fill <= y_end
-                        if y_fill <= y_fill_mid
-                            canvas[x + @x_neg_units, y_fill + @y_neg_units] = PNG::Color.new(255, 0, 0)
-                        else
-                            canvas[x + @x_neg_units, y_fill + @y_neg_units] = PNG::Color.new(255, 0, 0)
-                        end
-                    end
-                end
-            end
+            fill_empty_y(x, x_end, y_start, y_end, canvas)
         end
         png = PNG.new(canvas)
         png.save("graph.png")
